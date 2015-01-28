96f66146544522a6fcf83714a6e2fa669240f726
MRMajesticMR <mrmajesticmr90@gmail.com>
2015-01-28 16:36:41 +0300

    Добавлен код примера использования OpenGL2 в NDK

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,5 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := Asteroids_NDK
 LOCAL_SRC_FILES := Asteroids_NDK.cpp objects/GameObject.cpp objects/MoveableGameObject.cpp objects/Ship.cpp objects/Bullet.cpp objects/Asteroid.cpp
+LOCAL_LDLIBS    := -llog -lGLESv2
+LOCAL_CFLAGS    := -Werror
 
 include $(BUILD_SHARED_LIBRARY)
---
+++ src/ru/majestic/asteroids/GameActivity.java
@@ -0,0 +1,26 @@
+package ru.majestic.asteroids;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+	GL2JNIView mView;
+
+    @Override protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mView = new GL2JNIView(getApplication());
+        setContentView(mView);
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        mView.onPause();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        mView.onResume();
+    }
+
+}
